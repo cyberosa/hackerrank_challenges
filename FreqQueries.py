3f987e8c4deb088cd040197955b251518a98f653
cyberosa <arosacastillo@protonmail.com>
2020-09-12 13:02:11 +0200

    Added frequency of queries

---
+++ FreqQueries.py
@@ -0,0 +1,36 @@
+import os
+import re
+from collections import defaultdict
+
+
+def check_frequencies(n, list_commands):
+    stack_v = defaultdict(int)
+    stack_f = defaultdict(int)
+    for i in range(len(list_commands)):
+        c = int(list_commands[i][0])
+        v = int(list_commands[i][1])
+        if c == 1:
+            stack_f[stack_v[v]] -= 1 # previous value updated
+            stack_v[v]+=1
+            stack_f[stack_v[v]] += 1
+
+        elif c == 2:
+            if v in stack_v.keys():
+                stack_f[stack_v[v]] -= 1 # previous value updated
+                stack_v[v]-=1
+                stack_f[stack_v[v]] += 1
+            stack_v[v]  = 0 if stack_v[v] < 0 else stack_v[v]
+        else:
+            print('1' if v in stack_f and stack_f[v] > 0  else '0')
+
+if __name__ == '__main__':
+    #fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    commands = []
+
+    for _ in range(n):
+        commands.append(list(map(int, input().rstrip().split())))
+    
+    check_frequencies(n,commands)
